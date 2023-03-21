442fae684fab1af91cc81e5c3457f2b56441b379
Martyna Zając <martyna.sabina.zajac@gmail.com>
2023-03-21 21:27:44 +0100

    funkcje poprawione, dodałam repr z sortowaniem, ale tylko po imieniu

--- ZadanieWizytowki-wersjazfaker.py
+++ ZadanieWizytowki-wersjazfaker.py
@@ -10,7 +10,10 @@ class BaseContact:
         self.last_name = last_name
         self.phone_number = phone_number
         self.email = email
-        self._length=len(self.first_name) + len(self.last_name) + 1
+        self._length = len(self.first_name) + len(self.last_name) + 1
+
+    def __repr__(self):
+        return f"BaseContact(first_name={self.first_name}, last_name={self.last_name}, phone_number={self.phone_number}, email={self.email})"
 
     def __str__(self):
         return f"First name:{self.first_name}, Last name:{self.last_name}, phone:{self.phone_number}, e-mail:<{self.email}>"
@@ -30,9 +33,11 @@ class BusinessContact(BaseContact):
         self.company = company
         self.job = job
 
+    def __repr__(self):
+        return f"BusinessContact(first_name={self.first_name}, last_name={self.last_name}, phone_number={self.phone_number}, email={self.email}, work_phone={self.work_phone}, company={self.company}, job={self.job})"
+
     def contact(self):
         return f"Wybieram numer + 48 {self.work_phone} i dzwonię do {self.first_name} {self.last_name}"
-
 
 
 def create_contacts(kind, quantity):
@@ -59,38 +64,19 @@ def create_contacts(kind, quantity):
             )
             list_BusinessContact_faker.append(y)
             print(y)
-    return 'Done!'
+    return "Done!"
 
 
+def display_len(list):
+    for y in list:
+        print(f"{y.label_length}= {y.first_name} {y.last_name}")
+    return "Done!"
 
-def display_len(list_BaseContact_faker):
-    print("Length of name and surname:")
-    for x in list_BaseContact_faker:
-        print(f"{x.label_length}= {x.first_name} {x.last_name}")
 
-
-def display_contact_private(list_BaseContact_faker):
-    print("List of private contacts from BaseContact list:")
-    for x in list_BaseContact_faker:
+def display_contact(list):
+    for x in list:
         print(x.contact())
-
-
-# def display_contact_business(list_BusinessContact_faker):
-#     print("List of BusinessContacts:")
-#     for x in list_BusinessContact_faker:
-#         print(x.contact())
-
-
-def display_len_business(list_BusinessContact_faker):
-    print("Length of name and surname in BsinessContacts:")
-    for x in list_BusinessContact_faker:
-        print(f"{x.label_length}= {x.first_name} {x.last_name}")
-
-
-def display_contact_private_business(list_BusinessContact_faker):
-    print("List of private contacts from BusinessContact list:")
-    for x in list_BusinessContact_faker:
-        print(x.contact())
+    return "Done!"
 
 
 if __name__ == "__main__":
@@ -110,16 +96,24 @@ if __name__ == "__main__":
     )
     print(create_contacts(kind, quantity))
     if kind == 1:
+        print("\n---------------------------------------------------")
+        list_BaseContact_faker.sort(key=lambda x: x.first_name)
+        print(list_BaseContact_faker)
         print("\n\nInformation about people from BaseContact list:")
+        print("-----------------------------------------------------")
+        print("List of private contacts from BaseContact list:")
+        display_contact(list_BaseContact_faker)
         print("\n---------------------------------------------------")
-        display_contact_private(list_BaseContact_faker)
-        print("\n---------------------------------------------------")
+        print("Length of name and surname in BaseContacts:")
         display_len(list_BaseContact_faker)
     elif kind == 2:
+        print("\n---------------------------------------------------")
+        list_BusinessContact_faker.sort(key=lambda x: x.first_name)
+        print(list_BusinessContact_faker)
         print("\nInformation about people from BusinessContact list:")
+        print("------------------------------------------------------")
+        print("List of BusinessContacts:")
+        display_contact(list_BusinessContact_faker)
         print("\n---------------------------------------------------")
-        display_contact_private_business(list_BusinessContact_faker)
-        print("\n---------------------------------------------------")
-        # display_contact_business(list_BusinessContact_faker)
-        print("\n---------------------------------------------------")
-        display_len_business(list_BusinessContact_faker)
+        print("Length of name and surname in BsinessContacts:")
+        display_len(list_BusinessContact_faker)
